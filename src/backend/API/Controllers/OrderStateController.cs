37b9cafcb18ccbb9181d5955f55e8eb77b3a63dd
sk_kirill <k.skvortsov@artlogics.com>
2019-09-23 13:08:56 +0300

    вынес контроллеры

--- src/backend/API/Controllers/OrderStateController.cs
+++ src/backend/API/Controllers/OrderStateController.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Domain.Enums;
-using Domain.Services;
-using Domain.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,40 +7,5 @@ namespace API.Controllers
     public class OrderStateController :StateController<OrderState>
     {
         
-    }
-    
-    [Route("api/shippingState")]
-    public class ShippingStateController :StateController<ShippingState>
-    {
-        
-    }
-
-    public class StateController<T> :  Controller
-    {
-        /// <summary>
-        /// Все доступные статусы с цветами
-        /// </summary>
-        [HttpPost("search")]
-        public IEnumerable<StateDto> GetAll()
-        {
-            var values = Enum.GetValues(typeof(T));
-            var result = new List<StateDto>();
-            foreach (var value in values)
-            {
-                result.Add(new StateDto
-                {
-                    Name = value.ToString(),
-                    Color = AppColor.Blue.ToString()                    
-                });
-            }
-
-            return result;
-        }        
-    }
-
-    public class StateDto
-    {
-        public string Name { get; set; }
-        public string Color { get; set; }
     }
 }
