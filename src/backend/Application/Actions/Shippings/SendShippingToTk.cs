842f478ee242938e7068472c4274a33e69ce973d
sk_kirill <k.skvortsov@artlogics.com>
2019-09-25 17:09:20 +0300

    промежуточное решение по экшенам перевозок

---
+++ src/backend/Application/Actions/Shippings/SendShippingToTk.cs
@@ -0,0 +1,38 @@
+using DAL;
+using Domain;
+using Domain.Enums;
+using Domain.Persistables;
+using Domain.Services;
+
+namespace Application.Actions.Shippings
+{
+    /// <summary>
+    /// Отправить перевозку в ТК
+    /// </summary>
+    public class SendShippingToTk : IAppAction<Shipping>
+    {
+        private readonly AppDbContext db;
+        public AppColor Color { get; set; }
+
+        public SendShippingToTk(AppDbContext db)
+        {
+            this.db = db;
+            Color = AppColor.Blue;
+        }
+        public AppActionResult Run(User user, Shipping shipping)
+        {
+            shipping.Status = ShippingState.ShippingRequestSent;
+            db.SaveChanges();
+            return new AppActionResult
+            {
+                IsError = false,
+                Message = $"Перевозка {shipping.TransportationNumber} отправлена в ТК"
+            };
+        }
+
+        public bool IsAvailable(Role role, Shipping shipping)
+        {
+            return (shipping.Status == ShippingState.ShippingCreated) && (role.Name == "Administrator" || role.Name == "TransportCoordinator");
+        }
+    }
+}
