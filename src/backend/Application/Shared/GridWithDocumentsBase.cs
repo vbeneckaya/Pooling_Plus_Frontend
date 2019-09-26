c65168fa46246111c84e6c1ee3b7605db9ad92c5
Maxim Sirotkin <d.zh2012@yandex.ru>
2019-09-26 10:41:09 +0300

    Документы
    рефакторинг

---
+++ src/backend/Application/Shared/GridWithDocumentsBase.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using Domain.Persistables;
+using Domain.Services;
+using Domain.Services.Documents;
+using Domain.Services.UserIdProvider;
+using Domain.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace Application.Shared
+{
+    public abstract class GridWithDocumentsBase<TEntity, TDto> : GridServiceBase<TEntity, TDto>, IGridWithDocuments<TEntity, TDto> where TEntity : class, IPersistable, IWithDocumentsPersistable, new() where TDto : IDto
+    {
+        protected GridWithDocumentsBase(AppDbContext appDbContext, IUserIdProvider userIdProvider) : base(appDbContext, userIdProvider) { }
+
+        public ValidateResult CreateDocument(Guid id, DocumentDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DocumentDto> GetDocuments(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValidateResult DeleteDocument(Guid id, Guid documentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
