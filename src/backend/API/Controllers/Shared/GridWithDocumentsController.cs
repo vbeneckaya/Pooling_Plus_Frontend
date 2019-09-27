08e5bfd89d9005008e282d3029b8e88d622473a6
Maxim Sirotkin <d.zh2012@yandex.ru>
2019-09-27 10:26:15 +0300

    Документы
    обновление документа

--- src/backend/API/Controllers/Shared/GridWithDocumentsController.cs
+++ src/backend/API/Controllers/Shared/GridWithDocumentsController.cs
@@ -3,16 +3,29 @@ using Domain.Services;
 using Domain.Services.Documents;
 using Domain.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using System;
 using System.Collections.Generic;
 
 namespace API.Controllers.Shared
 {
-    public abstract class GridWithDocumentsController<TService, TEntity, TDto, TFormDto> : GridController<TService, TEntity, TDto, TFormDto> 
+    /// <summary>
+    /// Грид поддерживающий работу с документами
+    /// </summary>
+    /// <typeparam name="TService"></typeparam>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TDto"></typeparam>
+    /// <typeparam name="TFormDto"></typeparam>
+    public abstract class GridWithDocumentsController<TService, TEntity, TDto, TFormDto> : GridController<TService, TEntity, TDto, TFormDto>
         where TService : IGridWithDocuments<TEntity, TDto, TFormDto> where TEntity : IWithDocumentsPersistable, IPersistable
     {
         public GridWithDocumentsController(TService service) : base(service) { }
 
+        /// <summary>
+        /// Получить документы
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [Route("{id}/documents")]
         [HttpGet]
         public IActionResult GetDocuments(Guid id)
@@ -23,12 +36,19 @@ namespace API.Controllers.Shared
 
                 return Ok(documents);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
+                Log.Error(e, $"Failed to Get document");
                 return StatusCode(500);
             }
         }
 
+        /// <summary>
+        /// Создать документ
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dto"></param>
+        /// <returns></returns>
         [Route("{id}/documents")]
         [HttpPost]
         public IActionResult CreateDocument(Guid id, [FromBody] DocumentDto dto)
@@ -39,12 +59,43 @@ namespace API.Controllers.Shared
 
                 return Ok(result);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
+                Log.Error(e, $"Failed to Create document");
                 return StatusCode(500);
             }
         }
 
+        /// <summary>
+        /// Обновление документа
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="documentId"></param>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [Route("{id}/documents/{documentId}")]
+        [HttpPut]
+        public IActionResult UpdateDocument(Guid id, Guid documentId, [FromBody] DocumentDto dto)
+        {
+            try
+            {
+                ValidateResult result = service.UpdateDocument(id, documentId, dto);
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, $"Failed to Update document {documentId}");
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Удалить документ
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="documentId"></param>
+        /// <returns></returns>
         [Route("{id}/documents/{documentId}")]
         [HttpDelete]
         public IActionResult DeleteDocument(Guid id, Guid documentId)
@@ -57,6 +108,7 @@ namespace API.Controllers.Shared
             }
             catch (Exception e)
             {
+                Log.Error(e, $"Failed to Delete document {documentId}");
                 return StatusCode(500);
             }
         }
