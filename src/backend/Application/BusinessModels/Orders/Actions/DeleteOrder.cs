6da684dde4061f115f4e18c19c91baa9ce67c3fe
Andrey Suppes <a.suppes@artlogics.ru>
2019-10-11 14:33:42 +0500

    TMSL-57 - добавлено удаление заказа, рефакторинг actions

---
+++ src/backend/Application/BusinessModels/Orders/Actions/DeleteOrder.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using Domain;
+using Domain.Enums;
+using Domain.Persistables;
+using Domain.Services;
+using Domain.Services.Translations;
+using Domain.Services.UserProvider;
+using System.Linq;
+
+namespace Application.BusinessModels.Orders.Actions
+{
+    public class DeleteOrder : IAppAction<Order>
+    {
+        private readonly AppDbContext db;
+
+        public DeleteOrder(AppDbContext db)
+        {
+            this.db = db;
+            Color = AppColor.Red;
+        }
+
+        public AppColor Color { get; set; }
+
+        public AppActionResult Run(CurrentUserDto user, Order order)
+        {
+            string orderNumber = order.OrderNumber;
+
+            ///TODO: заменить на использованием флага IsActive после реализации фильтров (нужно будет прокидывать условие, что отбираем только активные для грида)
+            db.RemoveRange(db.OrderItems.Where(x => x.OrderId == order.Id));
+            db.RemoveRange(db.HistoryEntries.Where(x => x.PersistableId == order.Id));
+            db.Remove(order);
+
+            db.SaveChanges();
+
+            return new AppActionResult
+            {
+                IsError = false,
+                Message = "orderRemoved".translate(user.Language, orderNumber)
+            };
+        }
+
+        public bool IsAvailable(Role role, Order order)
+        {
+            return (order.Status == OrderState.Created || order.Status == OrderState.Draft) && (role.Name == "Administrator" || role.Name == "TransportCoordinator");
+        }
+    }
+}
