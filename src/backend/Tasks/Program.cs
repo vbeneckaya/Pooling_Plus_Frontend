0af6a5ae0242741882af25a858474bcc5746389f
Andrey Suppes <a.suppes@artlogics.ru>
2019-10-15 14:29:52 +0500

    отключение консоли везде, кроме отладки

--- src/backend/Tasks/Program.cs
+++ src/backend/Tasks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Threading;
 
 namespace Tasks
@@ -10,6 +11,14 @@ namespace Tasks
         {
             var culture = new CultureInfo("ru-RU");
             Thread.CurrentThread.CurrentCulture = culture;
+
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+            if (env != "Debug")
+            {
+                //disable console
+                Console.SetOut(TextWriter.Null);
+                Console.SetError(TextWriter.Null);
+            }
 
             return ConsoleShell.ForTasksInThisAssembly().Run(args);
         }
