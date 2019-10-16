d5b6907d477cbd4f97228491ddea22707035d555
vsmirnova <vsmirnova@mmtr.ru>
2019-10-16 12:23:24 +0300

    TMSL-106 TMSL-108

--- src/frontend/src/components/SuperGrid/components/header.jsx
+++ src/frontend/src/components/SuperGrid/components/header.jsx
@@ -1,8 +1,8 @@
-import React, {useRef, useState, useEffect} from 'react';
-import {Button, Dropdown, Form, Grid, Popup} from 'semantic-ui-react';
+import React, { useRef, useState, useEffect } from 'react';
+import { Button, Dropdown, Form, Grid, Popup } from 'semantic-ui-react';
 import Search from '../../../components/Search';
-import {useTranslation} from 'react-i18next';
-import {useSelector, useDispatch} from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { useSelector, useDispatch } from 'react-redux';
 import Select from '../../BaseComponents/Select';
 import {
     canImportFromExcelSelector,
@@ -12,24 +12,25 @@ import {
     importProgressSelector,
 } from '../../../ducks/gridList';
 
-import FieldsConfig from '../../FieldsConfig/index'
+import FieldsConfig from '../../FieldsConfig/index';
 import {
+    getRepresentationsRequest,
     representationNameSelector,
     representationsSelector,
-    setRepresentationRequest
-} from "../../../ducks/representations";
+    setRepresentationRequest,
+} from '../../../ducks/representations';
 
 const Header = ({
-                    createButton,
-                    searchValue,
-                    searchOnChange,
-                    counter,
-                    clearFilter,
-                    disabledClearFilter,
-                    loadList,
-                    name,
-                }) => {
-    const {t} = useTranslation();
+    createButton,
+    searchValue,
+    searchOnChange,
+    counter,
+    clearFilter,
+    disabledClearFilter,
+    loadList,
+    name,
+}) => {
+    const { t } = useTranslation();
 
     const dispatch = useDispatch();
 
@@ -43,7 +44,7 @@ const Header = ({
     const representations = useSelector(state => representationsSelector(state, name));
 
     const exportExcel = () => {
-        dispatch(exportToExcelRequest({name}));
+        dispatch(exportToExcelRequest({ name }));
     };
 
     const importExcel = () => {
@@ -55,7 +56,7 @@ const Header = ({
 
         const data = new FormData();
         data.append('FileName', file.name);
-        data.append('FileContent', new Blob([file], {type: file.type}));
+        data.append('FileContent', new Blob([file], { type: file.type }));
         data.append('FileContentType', file.type);
 
         dispatch(
@@ -67,61 +68,129 @@ const Header = ({
         );
     };
 
-    const settings = () => {
+    const getRepresentations = callBackFunc => {
+        dispatch(getRepresentationsRequest({ key: name, callBackFunc }));
     };
+
+    useEffect(
+        () => {
+            getRepresentations();
+        },
+        [name],
+    );
 
     const representation = useSelector(state => representationNameSelector(state, name));
 
-    const changeRepresentation = (key) => {
-        dispatch(setRepresentationRequest({
-            gridName: name,
-            value: key
-        }))
+    const changeRepresentation = key => {
+        dispatch(
+            setRepresentationRequest({
+                gridName: name,
+                value: key,
+            }),
+        );
     };
 
-    return <Grid className="grid-header-panel">
-        <Grid.Row>
-            <Grid.Column width={10}>
-                {createButton}
-                <Search value={searchValue} onChange={searchOnChange} isAuto/>
-                <span className="records-counter">
-                        {t('totalCount', {count: counter})}
-                    </span>
-            </Grid.Column>
-            <Grid.Column width={6} className="grid-right-elements">
-                <input type="file" ref={fileUploader} style={{display: 'none'}} onInput={onFilePicked}/>
-                <div className="representation">
-                    <label>{t('representation')}</label>
-                    <Form.Field>
-                        <Dropdown selection text={representation} fluid>
-                            <Dropdown.Menu>
-                                {representations && Object.keys(representations).length ? <>
-                                    {
-                                        Object.keys(representations).map(key => <Dropdown.Item text={key}
-                                                                                               onClick={() => changeRepresentation(key)}/>)
-                                    }
-                                    <Dropdown.Divider/>
-                                </> : null}
-                                <FieldsConfig title={t('Create representation')} gridName={name} isNew={true}>
-                                    <Dropdown.Item icon="add" text={t('create_btn')}/>
-                                </FieldsConfig>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Form.Field>
-                    <FieldsConfig title={t('Edit representation', {name: representation})} gridName={name}>
-                        <Button icon="cogs" disabled={!representation} onClick={settings}/>
-                    </FieldsConfig>
-                </div>
-                <Popup content={t('reset_filters')} position="bottom right"
-                       trigger={<Button icon="times" onClick={clearFilter} disabled={disabledClearFilter}/>}/>
-                {isImportBtn && <Popup content={t('importFromExcel')} position="bottom right"
-                                       trigger={<Button icon="upload" loading={importLoader} onClick={importExcel}/>}/>}
-                {true && <Popup content={t('exportToExcel') // todo
-                } position="bottom right"
-                                trigger={<Button icon="download" loading={exportLoader} onClick={exportExcel}/>}/>}
-            </Grid.Column>
-        </Grid.Row>
-    </Grid>;
+    return (
+        <Grid className="grid-header-panel">
+            <Grid.Row>
+                <Grid.Column width={10}>
+                    {createButton}
+                    <Search value={searchValue} onChange={searchOnChange} isAuto />
+                    <span className="records-counter">{t('totalCount', { count: counter })}</span>
+                </Grid.Column>
+                <Grid.Column width={6} className="grid-right-elements">
+                    <input
+                        type="file"
+                        ref={fileUploader}
+                        style={{ display: 'none' }}
+                        onInput={onFilePicked}
+                    />
+                    <div className="representation">
+                        <label>{t('representation')}</label>
+                        <Form.Field>
+                            <Dropdown
+                                selection
+                                text={representation || t('default_representation')}
+                                fluid
+                            >
+                                <Dropdown.Menu>
+                                    <Dropdown.Item
+                                        text={t('default_representation')}
+                                        onClick={() => changeRepresentation(null)}
+                                    />
+                                    {representations && Object.keys(representations).length ? (
+                                        <>
+                                            {Object.keys(representations).map(key => (
+                                                <Dropdown.Item
+                                                    text={key}
+                                                    onClick={() => changeRepresentation(key)}
+                                                />
+                                            ))}
+                                        </>
+                                    ) : null}
+                                    <Dropdown.Divider />
+                                    <FieldsConfig
+                                        title={t('Create representation')}
+                                        gridName={name}
+                                        isNew={true}
+                                        getRepresentations={getRepresentations}
+                                    >
+                                        <Dropdown.Item icon="add" text={t('create_btn')} />
+                                    </FieldsConfig>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Form.Field>
+                        <FieldsConfig
+                            title={t('Edit representation', { name: representation })}
+                            gridName={name}
+                            getRepresentations={getRepresentations}
+                        >
+                            <Button icon="cogs" disabled={!representation} />
+                        </FieldsConfig>
+                    </div>
+                    <Popup
+                        content={t('reset_filters')}
+                        position="bottom right"
+                        trigger={
+                            <Button
+                                icon="times"
+                                onClick={clearFilter}
+                                disabled={disabledClearFilter}
+                            />
+                        }
+                    />
+                    {isImportBtn && (
+                        <Popup
+                            content={t('importFromExcel')}
+                            position="bottom right"
+                            trigger={
+                                <Button
+                                    icon="upload"
+                                    loading={importLoader}
+                                    onClick={importExcel}
+                                />
+                            }
+                        />
+                    )}
+                    {true && (
+                        <Popup
+                            content={
+                                t('exportToExcel') // todo
+                            }
+                            position="bottom right"
+                            trigger={
+                                <Button
+                                    icon="download"
+                                    loading={exportLoader}
+                                    onClick={exportExcel}
+                                />
+                            }
+                        />
+                    )}
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
 };
 
 export default Header;
--- src/frontend/src/ducks/representations.js
+++ src/frontend/src/ducks/representations.js
@@ -1,13 +1,24 @@
 import { createSelector } from 'reselect';
-import { all, takeEvery, select } from 'redux-saga/effects';
+import { all, takeEvery, select, put } from 'redux-saga/effects';
 import { columnsGridSelector } from './gridList';
+import { postman } from '../utils/postman';
 
 //*  TYPES  *//
+const GET_REPRESENTATIONS_REQUEST = 'GET_REPRESENTATIONS_REQUEST';
+const GET_REPRESENTATIONS_SUCCESS = 'GET_REPRESENTATIONS_SUCCESS';
+const GET_REPRESENTATIONS_ERROR = 'GET_REPRESENTATIONS_ERROR';
+
 const SAVE_REPRESENTATION_REQUEST = 'SAVE_REPRESENTATION_REQUEST';
 const SAVE_REPRESENTATION_SUCCESS = 'SAVE_REPRESENTATION_SUCCESS';
 const SAVE_REPRESENTATION_ERROR = 'SAVE_REPRESENTATION_ERROR';
 
 const EDIT_REPRESENTATION_REQUEST = 'EDIT_REPRESENTATION_REQUEST';
+const EDIT_REPRESENTATION_SUCCESS = 'EDIT_REPRESENTATION_SUCCESS';
+const EDIT_REPRESENTATION_ERROR = 'EDIT_REPRESENTATION_ERROR';
+
+const DELETE_REPRESENTATION_REQUEST = 'DELETE_REPRESENTATION_REQUEST';
+const DELETE_REPRESENTATION_SUCCESS = 'DELETE_REPRESENTATION_SUCCESS';
+const DELETE_REPRESENTATION_ERROR = 'DELETE_REPRESENTATION_REQUEST';
 
 const SET_REPRESENTATION = 'SET_REPRESENTATION';
 
@@ -17,7 +28,7 @@ const REPRESENTATION_KEY = 'representation';
 //*  INITIAL STATE  *//
 
 const initial = {
-    list: JSON.parse(localStorage.getItem(REPRESENTATIONS_KEY)) || {},
+    list: {},
     representation: JSON.parse(localStorage.getItem(REPRESENTATION_KEY)) || {},
 };
 
@@ -27,28 +38,10 @@ export default (state = initial, { type, payload = {} }) => {
     const { gridName, name, value, oldName } = payload;
 
     switch (type) {
-        case SAVE_REPRESENTATION_REQUEST:
+        case GET_REPRESENTATIONS_SUCCESS:
             return {
                 ...state,
-                list: {
-                    ...state.list,
-                    [gridName]: {
-                        ...state.list[gridName],
-                        [name]: value,
-                    },
-                },
-            };
-        case EDIT_REPRESENTATION_REQUEST:
-            delete state.list[gridName][oldName];
-            return {
-                ...state,
-                list: {
-                    ...state.list,
-                    [gridName]: {
-                        ...state.list[gridName],
-                        [name]: value,
-                    },
-                },
+                list: payload,
             };
         case SET_REPRESENTATION:
             return {
@@ -65,6 +58,13 @@ export default (state = initial, { type, payload = {} }) => {
 
 //*  ACTION CREATORS  *//
 
+export const getRepresentationsRequest = payload => {
+    return {
+        type: GET_REPRESENTATIONS_REQUEST,
+        payload,
+    };
+};
+
 export const saveRepresentationRequest = payload => {
     return {
         type: SAVE_REPRESENTATION_REQUEST,
@@ -75,8 +75,8 @@ export const saveRepresentationRequest = payload => {
 export const editRepresentationRequest = payload => {
     return {
         type: EDIT_REPRESENTATION_REQUEST,
-        payload
-    }
+        payload,
+    };
 };
 
 export const setRepresentationRequest = payload => {
@@ -86,18 +86,18 @@ export const setRepresentationRequest = payload => {
     };
 };
 
+export const deleteRepresentationRequest = payload => {
+    return {
+        type: DELETE_REPRESENTATION_REQUEST,
+        payload
+    }
+};
+
 //*  SELECTORS *//
 
 export const stateSelector = state => state.representations;
 
-export const representationsSelector = createSelector(
-    [stateSelector, (state, name) => name],
-    (state, gridName) => {
-        const { list = {} } = state;
-
-        return list[gridName];
-    },
-);
+export const representationsSelector = createSelector(stateSelector, state => state.list);
 
 export const representationNameSelector = createSelector(
     [stateSelector, (state, name) => name],
@@ -110,7 +110,7 @@ export const representationSelector = createSelector(
     [stateSelector, (state, name) => name],
     (state, gridName) => {
         const representationName = state.representation[gridName];
-        return representationName ? state.list[gridName][representationName] : [];
+        return representationName ? state.list[representationName] : [];
     },
 );
 
@@ -119,34 +119,129 @@ export const representationFromGridSelector = createSelector(
     (state, gridName, list) => {
         const representationName = state.representation[gridName];
         if (representationName) {
-            return state.list[gridName][representationName];
+            console.log('rr', state.list[representationName]);
+            return state.list[representationName] || [];
         }
-        return list;
+        return list.filter(item => item.isDefault);
     },
 );
 
 //*  SAGA  *//
 
+function* getRepresentationsSaga({ payload }) {
+    try {
+        const { key, callBackFunc } = payload;
+        const result = yield postman.get(`/userSettings/${key}`);
+
+        yield put({
+            type: GET_REPRESENTATIONS_SUCCESS,
+            payload: result.value ? JSON.parse(result.value) : {},
+        });
+        callBackFunc && callBackFunc();
+    } catch (e) {
+        yield put({
+            type: GET_REPRESENTATIONS_ERROR,
+            payload: e,
+        });
+    }
+}
+
 function* saveRepresentationSaga({ payload }) {
     try {
-        const { callbackSuccess } = payload;
-        const state = yield select(state => state.representations.list);
-        localStorage.setItem(REPRESENTATIONS_KEY, JSON.stringify(state));
+        const { callbackSuccess, key, name, value } = payload;
+        const list = yield select(representationsSelector);
+
+        const params = {
+            ...list,
+            [name]: value,
+        };
+
+        const result = yield postman.post(`/userSettings/${key}`, {
+            value: JSON.stringify(params),
+        });
+
+        yield put({
+            type: SAVE_REPRESENTATION_SUCCESS
+        });
+
         callbackSuccess && callbackSuccess();
     } catch (e) {
-        console.log('___error', e);
+        yield put({
+            type: SAVE_REPRESENTATION_ERROR,
+            payload: e
+        })
+    }
+}
+
+function* editRepresentationSaga({ payload }) {
+    try {
+        const { callbackSuccess, key, name, value, oldName } = payload;
+        const list = yield select(representationsSelector);
+
+        let params = {};
+
+        Object.keys(list).forEach(keyList => {
+            if (keyList !== oldName) {
+                params = {
+                    ...params,
+                    [keyList]: list[keyList],
+                };
+            }
+        });
+
+        params = {
+            ...params,
+            [name]: value,
+        };
+
+        const result = yield postman.post(`/userSettings/${key}`, {
+            value: JSON.stringify(params),
+        });
+
+        yield put({
+            type: EDIT_REPRESENTATION_SUCCESS
+        });
+
+        callbackSuccess && callbackSuccess();
+    } catch (e) {
+        yield put({
+            type: EDIT_REPRESENTATION_ERROR,
+            payload: e
+        })
     }
 }
 
 
-function* editRepresentationSaga({ payload }) {
+function* deleteRepresentationSaga({ payload }) {
     try {
-        const { callbackSuccess } = payload;
-        const state = yield select(state => state.representations.list);
-        localStorage.setItem(REPRESENTATIONS_KEY, JSON.stringify(state));
+        const { callbackSuccess, key, name } = payload;
+        const list = yield select(representationsSelector);
+
+        let params = {};
+
+        Object.keys(list).forEach(keyList => {
+            if (keyList !== name) {
+                params = {
+                    ...params,
+                    [keyList]: list[keyList],
+                };
+            }
+        });
+
+        const result = yield postman.post(`/userSettings/${key}`, {
+            value: JSON.stringify(params),
+        });
+
+        yield put({
+            type: DELETE_REPRESENTATION_SUCCESS
+        });
+
         callbackSuccess && callbackSuccess();
     } catch (e) {
-        console.log('___error', e);
+        yield put({
+            type: DELETE_REPRESENTATION_ERROR,
+            payload: e
+        })
     }
 }
 
@@ -163,6 +258,8 @@ export function* saga() {
     yield all([
         takeEvery(SAVE_REPRESENTATION_REQUEST, saveRepresentationSaga),
         takeEvery(EDIT_REPRESENTATION_REQUEST, editRepresentationSaga),
+        takeEvery(DELETE_REPRESENTATION_REQUEST, deleteRepresentationSaga),
         takeEvery(SET_REPRESENTATION, setRepresentationSaga),
+        takeEvery(GET_REPRESENTATIONS_REQUEST, getRepresentationsSaga),
     ]);
 }
