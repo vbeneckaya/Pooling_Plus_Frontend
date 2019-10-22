0dafbd9e270e6eb278dc5463701b4066594e5df6
Andrey Suppes <a.suppes@artlogics.ru>
2019-10-22 16:02:08 +0500

    TMSL-144 - убраны дубли из фильтра по SoldTo

--- src/backend/Application/Services/Warehouses/SoldToService.cs
+++ src/backend/Application/Services/Warehouses/SoldToService.cs
@@ -10,9 +10,15 @@ namespace Application.Services.Warehouses
     {
         public IEnumerable<SoldToDto> ForSelect()
         {
+            HashSet<string> addedSoldTo = new HashSet<string>();
             var warehouses = _db.Warehouses.Where(w => w.SoldToNumber != null && w.SoldToNumber.Length > 0).OrderBy(w => w.SoldToNumber).ToList();
             foreach (Warehouse wh in warehouses)
             {
+                if (addedSoldTo.Contains(wh.SoldToNumber))
+                {
+                    continue;
+                }
+                addedSoldTo.Add(wh.SoldToNumber);
                 SoldToDto dto = new SoldToDto
                 {
                     Name = $"{wh.SoldToNumber} ({wh.WarehouseName})",
