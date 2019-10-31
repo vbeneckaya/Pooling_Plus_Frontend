8d62e0ebb97b422bca25214b3b0cd3fb2b964d50
Semen Gordeev <s.gordeev@artlogics.ru>
2019-10-31 22:36:46 +0300

    TMSL-163 добавлено поле OrderConfirmed

--- src/backend/Domain/Persistables/Order.cs
+++ src/backend/Domain/Persistables/Order.cs
@@ -239,6 +239,14 @@ namespace Domain.Persistables
             return OrderNumber;
         }
 
+        /// <summary>
+        /// Дата изменения
+        /// </summary>
         public DateTime? OrderChangeDate { get; set; }
+
+        /// <summary>
+        /// Заказ подтвержден
+        /// </summary>
+        public bool OrderConfirmed { get; set; }
     }
 }
