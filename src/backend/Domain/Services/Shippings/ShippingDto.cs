9c32aa95116e5d3eb5be0898928915b67c324d4a
Andrey Suppes <a.suppes@artlogics.ru>
2019-11-06 19:09:56 +0500

    TMSL-164 - исправлена ошибка при сохранении

--- src/backend/Domain/Services/Shippings/ShippingDto.cs
+++ src/backend/Domain/Services/Shippings/ShippingDto.cs
@@ -126,7 +126,7 @@ namespace Domain.Services.Shippings
         [FieldType(FieldType.Boolean)]
         public bool? CostsConfirmedByCarrier { get; set; }
 
-        [FieldType(FieldType.DateTime), IsDefault, OrderNumber(6)]
+        [FieldType(FieldType.DateTime), IsDefault, OrderNumber(6), IgnoreFieldSettings]
         public string ShippingCreationDate { get; set; }
         /*end of fields*/
     }
