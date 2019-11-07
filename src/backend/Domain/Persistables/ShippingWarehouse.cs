98afb63e8730d6c6144adbb2f4e24769d0122bbb
Andrey Suppes <a.suppes@artlogics.ru>
2019-11-07 12:12:09 +0500

    TMSL-206 - Создать справочник Склады отгрузки

---
+++ src/backend/Domain/Persistables/ShippingWarehouse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Domain.Persistables
+{
+    /// <summary>
+    /// Склад отгрузки
+    /// </summary>
+    public class ShippingWarehouse : IPersistable
+    {
+        /// <summary>
+        /// Db primary key
+        /// </summary>    
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Код
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Наименование склада
+        /// </summary>
+        public string WarehouseName { get; set; }
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Распознанный адрес
+        /// </summary>
+        public string ValidAddress { get; set; }
+        /// <summary>
+        /// Индекс
+        /// </summary>
+        public string PostalCode { get; set; }
+        /// <summary>
+        /// Регион
+        /// </summary>
+        public string Region { get; set; }
+        /// <summary>
+        /// Район
+        /// </summary>
+        public string Area { get; set; }
+        /// <summary>
+        /// Населенный пункт
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// Улица
+        /// </summary>
+        public string Street { get; set; }
+        /// <summary>
+        /// Дом
+        /// </summary>
+        public string House { get; set; }
+        /// <summary>
+        /// Активный
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
---
+++ src/backend/Domain/Services/ShippingWarehouses/ShippingWarehouseDto.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Enums;
+using Domain.Extensions;
+
+namespace Domain.Services.ShippingWarehouses
+{
+    public class ShippingWarehouseDto : IDto
+    { 
+        public string Id { get; set; }
+
+        [FieldType(FieldType.Text), OrderNumber(1)]
+        public string Code { get; set; }
+
+        [FieldType(FieldType.Text), OrderNumber(2)]
+        public string WarehouseName { get; set; }
+
+        [FieldType(FieldType.Text), OrderNumber(3)]
+        public string Address { get; set; }
+
+        public string ValidAddress { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Region { get; set; }
+
+        public string Area { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string House { get; set; }
+
+        [FieldType(FieldType.Boolean), OrderNumber(4)]
+        public bool? IsActive { get; set; }
+    }
+}
