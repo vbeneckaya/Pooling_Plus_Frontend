67db8a7378b160291dbbbe50c9df494dd2bee026
Semen <s.gordeev@artlogics.ru>
2019-11-08 15:26:10 +0300

    TMSL-87 косметика

--- src/backend/Domain/Shared/ImportResult.cs
+++ src/backend/Domain/Shared/ImportResult.cs
@@ -8,124 +8,36 @@ namespace Domain.Shared
 {
     public class ImportResult
     {
-        private readonly string _locale;
-
         public List<ValidateResult> Results { get; } = new List<ValidateResult>();
 
-        public int ErrorsCount
-        {
-            get 
-            {
-                return this.Results.Where(i => i.IsError).Count();
-            }
-        }
-        
-        public int SuccessCount
-        {
-            get
-            {
-                return this.Results.Where(i => !i.IsError).Count();
-            }
-        }
+        public int ErrorsCount => this.Results.Where(i => i.IsError).Count();
 
-        public int UpdatedCount
-        {
-            get
-            {
-                return this.Results.Where(i => i.ResultType == ValidateResultType.Updated).Count();
-            }
-        }
+        public int SuccessCount => this.Results.Where(i => !i.IsError).Count();
 
-        public int CreatedCount
-        {
-            get
-            {
-                return this.Results.Where(i => i.ResultType == ValidateResultType.Created).Count();
-            }
-        }
+        public int UpdatedCount => this.Results.Where(i => i.ResultType == ValidateResultType.Updated).Count();
 
-        private IEnumerable<DetailedValidattionResult> DetailedResults
-        {
-            get 
-            {
-                return this.Results
+        public int CreatedCount => this.Results.Where(i => i.ResultType == ValidateResultType.Created).Count();
+
+        private IEnumerable<DetailedValidattionResult> DetailedResults => this.Results
                         .Where(i => i.GetType() == typeof(DetailedValidattionResult))
                         .Cast<DetailedValidattionResult>();
-            }
-        }
 
-        public int RequiredErrorsCount
-        {
-            get
-            {
-                return this.DetailedResults
+        public int RequiredErrorsCount => this.DetailedResults
                     .Where(i => i.Errors.Any(e => e.ResultType == ValidationErrorType.ValueIsRequired))
                     .Count();
-            }
-        }
 
-        public int InvalidDictionaryValueErrorsCount
-        {
-            get
-            {
-                return this.DetailedResults
+        public int InvalidDictionaryValueErrorsCount => this.DetailedResults
                     .Where(i => i.Errors.Any(e => e.ResultType == ValidationErrorType.InvalidDictionaryValue))
                     .Count();
-            }
-        }
 
-        public int DuplicatedRecordErrorsCount
-        {
-            get
-            {
-                return this.DetailedResults
+        public int DuplicatedRecordErrorsCount => this.DetailedResults
                     .Where(i => i.Errors.Any(e => e.ResultType == ValidationErrorType.DuplicatedRecord))
                     .Count();
-            }
-        }
 
-        public int InvalidValueFormatErrorsCount
-        {
-            get
-            {
-                return this.DetailedResults
+        public int InvalidValueFormatErrorsCount => this.DetailedResults
                     .Where(i => i.Errors.Any(e => e.ResultType == ValidationErrorType.InvalidValueFormat))
                     .Count();
-            }
-        }
 
-        public string RequiredErrorsMessage
-        {
-            get
-            {
-                return "requiredErrorsMessage".Translate(_locale, RequiredErrorsCount);
-            }
-        }
-
-        public string InvalidDictionaryValueErrorsMessage
-        {
-            get
-            {
-                return "invalidDictionaryValueErrorsMessage".Translate(_locale, InvalidDictionaryValueErrorsCount);
-            }
-        }
-
-        public string DuplicatedRecordErrorsMEssage
-        {
-            get
-            {
-                return "invalidDictionaryValueErrorsMessage".Translate(_locale, RequiredErrorsCount);
-            }
-        }
-
-
-
-        public bool IsError 
-        {
-            get 
-            {
-                return Results.Any(i => i.IsError);
-            }
-        }
+        public bool IsError => Results.Any(i => i.IsError);
     }
 }
