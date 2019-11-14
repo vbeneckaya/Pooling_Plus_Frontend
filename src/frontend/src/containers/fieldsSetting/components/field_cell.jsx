93befd78fb9f950e25ac0d82c5ffbc5e01431066
vsmirnova <vsmirnova@mmtr.ru>
2019-11-14 13:35:31 +0300

    optimization

---
+++ src/frontend/src/containers/fieldsSetting/components/field_cell.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FieldCell = () => {
+
+};
+
+export default FieldCell;
---
+++ src/frontend/src/containers/fieldsSetting/components/setting_cell.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SettingCell = () => {
+
+};
+
+export default SettingCell;
