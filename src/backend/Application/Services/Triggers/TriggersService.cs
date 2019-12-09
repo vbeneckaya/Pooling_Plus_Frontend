7a3e4829d6b19f59ed487866997337e7cc4fb116
Andrey Suppes <a.suppes@artlogics.ru>
2019-12-09 13:55:22 +0500

    TMSL-301 - правка определения списка изменений для триггеров

--- src/backend/Application/Services/Triggers/TriggersService.cs
+++ src/backend/Application/Services/Triggers/TriggersService.cs
@@ -1,6 +1,7 @@
 ﻿using Application.BusinessModels.Shared.Triggers;
 using DAL.Services;
 using Domain.Persistables;
+using Domain.Shared;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -21,19 +22,22 @@ namespace Application.Services.Triggers
 
         public void Execute()
         {
-            RunTriggers<Order>();
-            RunTriggers<Shipping>();
-            RunTriggers<Tariff>();
+            var orderChanges = _dataService.GetChanges<Order>().ToList();
+            var shippingChanges = _dataService.GetChanges<Shipping>().ToList();
+            var tariffChanges = _dataService.GetChanges<Tariff>().ToList();
+
+            _dataService.SaveChanges();
+
+            RunTriggers(orderChanges);
+            RunTriggers(shippingChanges);
+            RunTriggers(tariffChanges);
         }
 
-        private void RunTriggers<TEntity>() where TEntity : class, IPersistable
+        private void RunTriggers<TEntity>(IEnumerable<EntityChangesDto<TEntity>> changes) where TEntity : class, IPersistable
         {
             var triggers = _serviceProvider.GetService<IEnumerable<ITrigger<TEntity>>>();
             if (triggers.Any())
             {
-                var changes = _dataService.GetChanges<TEntity>().ToList();
-                _dataService.SaveChanges();
-
                 foreach (var entityChanges in changes)
                 {
                     foreach (var trigger in triggers)
