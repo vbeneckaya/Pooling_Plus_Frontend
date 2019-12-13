4b42b124b5a4244ad98202a323b93d00da07491e
sk_kirill <k.skvortsov@artlogics.com>
2019-12-13 16:08:08 +0300

    Логируем изменение типа ТС у заказов из перевозки

--- src/backend/Application/BusinessModels/Shippings/Triggers/OnChangeVehicleTypeId.cs
+++ src/backend/Application/BusinessModels/Shippings/Triggers/OnChangeVehicleTypeId.cs
@@ -3,16 +3,24 @@ using DAL.Services;
 using Domain.Persistables;
 using Domain.Shared;
 using System.Linq;
+using Application.Services.Shippings;
+using DAL.Queries;
+using Domain.Extensions;
+using Domain.Services.History;
 
 namespace Application.BusinessModels.Shippings.Triggers
 {
     public class OnChangeVehicleTypeId : ITrigger<Shipping>
     {
         private readonly ICommonDataService _dataService;
+        private readonly IHistoryService _historyService;
+        private readonly IDeliveryCostCalcService _calcService;        
 
-        public OnChangeVehicleTypeId(ICommonDataService dataService)
+        public OnChangeVehicleTypeId(ICommonDataService dataService, IHistoryService historyService, IDeliveryCostCalcService calcService)
         {
             _dataService = dataService;
+            _historyService = historyService;
+            _calcService = calcService;
         }
 
         public void Execute(Shipping entity)
@@ -20,9 +28,24 @@ namespace Application.BusinessModels.Shippings.Triggers
             var orders = _dataService.GetDbSet<Order>()
                 .Where(x => x.ShippingId == entity.Id);
 
+            var vehicleTypes = _dataService.GetDbSet<VehicleType>();
+            
             foreach (var orderInShipping in orders)
             {
+                VehicleType oldVehicleType = null;
+                VehicleType newVehicleType  = null;
+                        
+                if (orderInShipping.VehicleTypeId.HasValue)
+                    oldVehicleType = vehicleTypes.GetById(orderInShipping.VehicleTypeId.Value);
+
+                if (entity.VehicleTypeId.HasValue)
+                    newVehicleType = vehicleTypes.GetById(entity.VehicleTypeId.Value);
+                        
                 orderInShipping.VehicleTypeId = entity.VehicleTypeId;
+                        
+                _historyService.Save(orderInShipping.Id, "fieldChanged",
+                    nameof(entity.VehicleTypeId).ToLowerFirstLetter(),
+                    oldVehicleType, newVehicleType);
             }
         }
 
