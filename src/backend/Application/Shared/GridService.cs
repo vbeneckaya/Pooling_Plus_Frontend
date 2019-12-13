de0724a7566719d46585f3853abdf5df8f39941b
Andrey Suppes <a.suppes@artlogics.ru>
2019-12-13 16:59:48 +0500

    Revert "Revert "TMSL-305 - actions только для всех выбранных вариантов""
    This reverts commit a569841a9bd6e15bc25312f232c0e9cf9cb68021.

--- src/backend/Application/Shared/GridService.cs
+++ src/backend/Application/Shared/GridService.cs
@@ -361,7 +361,7 @@ namespace Application.Shared
                 }
 
                 var validEntities = entities.Where(e => action.IsAvailable(e));
-                if (validEntities.Any())
+                if (validEntities.Any() && ids.Count() == validEntities.Count())
                 {
                     var actionDto = result.FirstOrDefault(x => x.Name == actionName);
                     if (actionDto == null)
