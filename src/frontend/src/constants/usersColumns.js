ead8d88c039ec90346ff88f1505a7392fdd8601c
vsmirnova <vsmirnova@mmtr.ru>
2019-12-18 11:20:37 +0300

    fix columnName

--- src/frontend/src/constants/usersColumns.js
+++ src/frontend/src/constants/usersColumns.js
@@ -3,21 +3,26 @@ import { ACTIVE_TYPE, ENUM_TYPE, SELECT_TYPE } from './columnTypes';
 export const usersColumns = [
     {
         name: 'email',
+        displayNameKey: 'email',
     },
     {
         name: 'userName',
+        displayNameKey: 'userName',
     },
     {
         name: 'role',
+        displayNameKey: 'role',
         isTranslate: true,
     },
     {
         name: 'carrierId',
+        displayNameKey: 'carrierId',
         type: SELECT_TYPE,
         source: 'transportCompanies',
     },
     {
         name: 'isActive',
+        displayNameKey: 'isActive',
         type: ACTIVE_TYPE,
     },
 ];
