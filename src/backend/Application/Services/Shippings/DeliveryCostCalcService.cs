a802f0aca02e67c9a0da8ee82369e620e0848fa3
sk_kirill <k.skvortsov@artlogics.com>
2020-02-06 14:38:01 +0300

    умножаем паллеты

--- src/backend/Application/Services/Shippings/DeliveryCostCalcService.cs
+++ src/backend/Application/Services/Shippings/DeliveryCostCalcService.cs
@@ -116,7 +116,7 @@ namespace Application.Services.Shippings
             else
             {
                 int totalPallets = orders.Sum(x => x.ConfirmedPalletsCount ?? x.ActualPalletsCount ?? x.PalletsCount ?? 0);
-                cost = GetLtlRate(tariff, totalPallets) ?? 0M;
+                cost = GetLtlRate(tariff, totalPallets)*totalPallets ?? 0M;
             }
 
             return cost;
