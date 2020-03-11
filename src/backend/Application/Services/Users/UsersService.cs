451afbae0eeee64ed98a31dcf3e35388bac054cc
vbeneckaya <vbeneckaya@VBENECKAYA>
2020-03-11 16:25:55 +0300

    ссылка для быстрого входа

--- src/backend/Application/Services/Users/UsersService.cs
+++ src/backend/Application/Services/Users/UsersService.cs
@@ -286,7 +286,7 @@ namespace Application.Services.Users
                     .ForMember(t => t.Role, e=>e.MapFrom((s) =>  roles.FirstOrDefault(role => role.Id == s.RoleId).Name))
                     
                     .ForMember(t => t.SignWithoutLoginLink, e => e.MapFrom((s) => 
-                        $"https://plus.pooling.me/signWithoutLoginLink/{s.Id.ToString()}/{Uri.EscapeDataString(s.PasswordHash.GetHash())}"
+                        $"https://plus.pooling.me/signwithoutlogin/{s.Id.ToString()}/{Uri.EscapeDataString(s.PasswordHash.GetHash())}"
                         ))
                     
                     .ForMember(t => t.CarrierId, e => e.MapFrom( s => s.CarrierId == null ? null : new LookUpDto(s.CarrierId.ToString())))
