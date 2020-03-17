50c784fc2db796de8585abbae7351e28db9e0240
vbeneckaya <vbeneckaya@VBENECKAYA>
2020-03-17 18:30:35 +0300

    menu

---
+++ src/frontend/src/components/InstructionModal/instructionContent.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const InstructionContent = (pathName) => {
+    const content = new Object();
+
+    content['/grid/orders'] = {
+        title: "Заказы",
+        content:
+            <label>instruction for /grid/orders</label>
+    };
+
+    content['/grid/shippings'] = {
+        title: "Перевозки",
+        content:
+            <label>instruction  for /grid/shippings</label>
+    };
+
+    const res = content[pathName]
+    
+    return res;
+}
+
+export default InstructionContent;
