5585cd54e7158a6a3d643d1d89124785de50b7b3
vbeneckaya <vbeneckaya@VBENECKAYA>
2020-03-18 19:12:55 +0300

    показ инструкции

--- src/frontend/src/ducks/general.js
+++ src/frontend/src/ducks/general.js
@@ -1,13 +1,8 @@
-import {select, take, spawn, all, put, takeEvery} from 'redux-saga/effects';
+import {select, take, spawn, put} from 'redux-saga/effects';
 import {createSelector} from 'reselect';
 import {clearDictionaryInfo} from './dictionaryView';
 import {autoUpdateStop} from './gridList';
 import React from "react";
-// import InstructionContent from "../components/InstructionModal/instructionContent";
-//
-// const SHOW_INSTRUCTION = 'SHOW_INSTRUCTION';
-//
-// const HIDE_INSTRUCTION = 'HIDE_INSTRUCTION';
 
 //*  INITIAL STATE  *//
 
@@ -15,29 +10,9 @@ const initial = {
     card: null,
 };
 
-// export default (state = initial, {type, payload}) => {
-//     switch (type) {
-//         case SHOW_INSTRUCTION:
-//             return {
-//                 ...state,
-//                 card: payload,
-//             };
-//         case HIDE_INSTRUCTION:
-//             return {
-//                 ...state,
-//                 card: null,
-//             };
-//
-//         default:
-//             return state;
-//     }
-// };
-
 const routeSelector = state => state.router;
-// const instructionSelector = state => state.instructions;
 
 const currentLocationSelector = createSelector(routeSelector, state => state.location.pathname);
-// export const showInstructionSelector = createSelector(instructionSelector, state => state.card);
 
 function* changeLocation() {
     while (true) {
@@ -46,31 +21,6 @@ function* changeLocation() {
         const {location} = payload;
         const {pathname} = location;
 
-        // if(pathname !== currentLocation) {
-        //
-        //     const alreadyInLocalStorage = localStorage.getItem(pathname);
-        //
-        //     if (!alreadyInLocalStorage) {
-        //
-        //         const content = InstructionContent(pathname);
-        //
-        //         if (!content) {
-        //             yield put({
-        //                 type: SHOW_INSTRUCTION,
-        //                 payload: null,
-        //             });
-        //         }
-        //
-        //         else {
-        //             localStorage.setItem(pathname, '1');
-        //             yield put({
-        //                 type: SHOW_INSTRUCTION,
-        //                 payload: content,
-        //             });
-        //         }
-        //     }
-        // }
-        
         if (pathname.includes('dictionary') && pathname !== currentLocation) {
             yield put(clearDictionaryInfo());
         }
@@ -84,19 +34,6 @@ function* changeLocation() {
         }
     }
 }
-
-// export const hideInstruction = payload => {
-//     return {
-//         type: HIDE_INSTRUCTION,
-//         payload,
-//     };
-// };
-//
-// function* hideInstructionSaga() {
-//     yield put({
-//         type: HIDE_INSTRUCTION
-//     });
-// }
 
 export function* saga() {
     yield spawn(changeLocation);
